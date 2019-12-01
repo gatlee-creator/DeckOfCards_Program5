4b0a3da9af9ae11bd8809396dc823733660c6152
gatlee-creator <gatlee@highpoint.edu>
2019-12-01 11:40:12 -0500

    card deck header class setup

---
+++ card_deck.h
@@ -0,0 +1,44 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class CardDeck
+{
+    public:
+        CardDeck();
+        /*
+            Default constructor. This will intialize the 
+            deck with hard code values for the faces and suit.
+            It will also set face and suit to 0. 
+        */ 
+        string getFace();
+        /*
+            This function will return the face value
+            of the card on top of the deck after it has
+            been shuffled.
+            Precondition:
+                - deck is intialized
+                - shuffleDeck has been called 
+        */
+        string getSuit(); 
+        /*
+            This function will return the suit of the card
+            on top of the deck after it has been shuffled.
+            Precondition:
+                - deck is intialized
+                - shuffleDeck has been called 
+        */
+        void shuffleDeck(); 
+        /*
+            This function will shuffle both the vector arrays
+            faces and suit to randomize the deck. 
+        */
+
+        private:
+            int face;
+            int suit; 
+
+            const vector<string> faces[13];
+            const vector<string> suit[4];
+
+};
