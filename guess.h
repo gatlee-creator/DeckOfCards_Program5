79147a432579b26c13dcd32682b0e2afc028c715
gatlee-creator <gatlee@highpoint.edu>
2019-12-03 11:17:33 -0500

    added Guess class

---
+++ guess.h
@@ -0,0 +1,46 @@
+/*
+    Name: Graham Atlee
+    Course: csc1720
+    Date: 12/3/2019
+
+    Header file for Guess class 
+*/
+#include <string>
+using namespace std;
+
+class Guess
+{
+    public:
+        int guessFace();
+        /*
+            This function will prompt the user 
+            to enter a guess for what the face value 
+            will be on top of the deck.
+            Preconditions:
+                - input must be a string ex. "Queen"
+                - input can lower or upper case
+            If the guess is wrong then a -1 will be returned 
+        */
+        int guessSuit();
+        /*
+            This function will prompt the user 
+            to enter a guess for what the suit value
+            will be on top of the deck.
+            Preconditions:
+                - input must be a string ex. "Hearts"
+                - input can lower or upper case
+            If the guess is wrong then a -1 will be returned 
+        */
+        int guessFaceAndSuit(); 
+        /*
+            This function will prompt the user 
+            to enter a guess for what the face and
+            suit values will be be on top of the deck.
+            Preconditions:
+                - input must be a string ex. "Queen"
+                - input can lower or upper case
+            If the guess is wrong then a -1 will be returned 
+        */
+       private:
+            string guess;
+};
---
+++ guessImp.cpp
@@ -0,0 +1,16 @@
+// #include "guess.h"
+// #include "cardDeck.h"
+// #include <iostream>
+
+// int Guess::guessFace(){
+//     CardDeck deck; 
+
+//      cout << "ENTER GUESS: " << endl;
+//      cin >> guess; 
+
+//      if(guess == deck.getFace())
+//          cout << "Match" << endl;
+//     else 
+//         return -1; 
+    
+// }
