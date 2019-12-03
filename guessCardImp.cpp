fa4bd38ac033ef6aabb3056ad14ac1d91cf262ed
atlee19 <atleegraham14@gmail.com>
2019-12-03 15:33:59 -0500

    added header documentation for guessCardImp

--- guessCardImp.cpp
+++ guessCardImp.cpp
@@ -1,6 +1,18 @@
+/*
+    Name: Graham Atlee
+    Course: csc1720
+    Date: 12/3/2019
+
+    Header file for Guess class 
+*/
 #include "guessCard.h"
 #include <iostream>
 
+/*
+    This function takes input from the user to 
+    check if it matches the face value on top
+    of the deck. 
+*/
 int GuessCard::guessFace(){
     
      cout << "ENTER GUESS: " << endl;
