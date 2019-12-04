b12f807bfe3ea35a1da45aaa92a1cfdc273c0f14
gatlee-creator <gatlee@highpoint.edu>
2019-12-04 18:27:48 -0500

    renamed test file and added documentation

---
+++ Atlee_main.cpp
@@ -0,0 +1,18 @@
+/*
+    Name: Graham Atlee
+    Course: csc1720
+    Date: 12/4/2019
+
+    Main program for the card guessing game. 
+*/
+#include <iostream>
+#include "cardGame.h"
+
+using namespace std; 
+
+int main()
+{
+    CardGame game;
+
+    game.play(); //starts the game 
+}
